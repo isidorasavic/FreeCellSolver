bf717d70c6b509922231e602fd09d2ded7d41fb5
isidorasavic <isidora99savic@gmail.com>
2022-06-30 00:00:21 +0200

    image processing

---
+++ Board.py
@@ -0,0 +1,7 @@
+class Board:
+    cards = [8][7]
+
+    def __init__(self, cards):
+        i = 0
+        j = 0
+        for card in cards:
---
+++ Card.py
@@ -0,0 +1,9 @@
+class Card:
+    number = -1
+    symbol = "none"
+    colour = "none"
+
+    def __init__(self, number, colour, symbol):
+        self.number = number
+        self.colour = colour
+        self.symbol = symbol
