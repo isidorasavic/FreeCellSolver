abe764820d19622a10ccf64d4e1625fe784b432a
Isidora <69584865+isidorasavic@users.noreply.github.com>
2022-07-03 11:26:13 +0200

    removed old classes

--- Card.py
+++
@@ -1,9 +0,0 @@
-class Card:
-    number = -1
-    symbol = "none"
-    colour = "none"
-
-    def __init__(self, number, colour, symbol):
-        self.number = number
-        self.colour = colour
-        self.symbol = symbol
